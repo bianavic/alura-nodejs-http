df1c7f163207621609f168b2afcd042acaf8b441
bianavic <bianavic@gmail.com>
2019-10-02 12:57:43 -0300

    implementado POST e utilizando curl

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,13 +1,18 @@
 // carregando express e iniciando a aplicacao que inicialmente estava no index.js
 const express = require('express');
 const consign = require('consign');
+const bodyParser = require('body-parser');
 
 module.exports = function() {
     var app = express();
 
-    // consig ajuda no gerenciamento das pastas, ajuda no carregamento das rotas
-    // esta lib é mais leve e independente do framework
-    // abaixo foi definido o que queremos incluir no objeto express (dentro da variavel app)
+    // adicionar o middleware body-parser no express, agora esta pronto para receber json
+    app.use(bodyParser.json());
+
+    // consig carrega todos os arquivos da pasta controllers para dentro do objeto express
+    // ele ajuda no carregamento das rotas
+    // é uma lib é mais leve e independente do framework
+    // abaixo definimos o que incluiremos no objeto express (dentro da variavel app)
     // desta forma a variavel app passa a deter todo o conhecimento da pasta controllers
     consign()
     .include('controllers')
--- index.js
+++ index.js
@@ -4,14 +4,9 @@ app.listen(3010, function(){
     console.log('Tá rolando na porta 3010'); // esta funfando mas ainda nao tem rota
 });
 
-/* app.get('/teste', function(req, res){
-    console.log('Recebida requisição do teste') // resposta no console para o dev
-    res.send('OK') // esta é a resposta para o cliente
-})
-
-// codigo abaixo transferido para controllers/pagamento.js para organizar rotas
-app.get('/pagamentos', function(req, res) {
-    console.log('rota pagamentos criada')
+// receber dados para criar pagamento
+app.post('/pagamentos/pagamento', function(req, res) {
+    var pagamento = req.body;
+    console.log(pagamento);
     res.send('OK')
-}) */
-
+});
